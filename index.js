b6da9e51c0b9d91be230e32584262bfbabbdfd53
ghrairi-ahmed <ghrayriahmed88@gmail.com>
2023-02-12 10:23:01 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import router from "./router/router.js";
+import router from "./router.js";
 export const app = express()
 const port = 3000
 app.set("view engine", "ejs" )
